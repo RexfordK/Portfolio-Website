f278ccbf37b15319b7ac6369443ea8767efa5d34
Rexford <rex982@live.com>
2019-01-04 00:55:40 -0500

    sass and gulp is set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 var gulp = require("gulp");
-var uglify = require('gulp-uglify');
-var pump = require('pump');
+var uglify = require("gulp-uglify");
+var sass = require("gulp-sass");
 
-gulp.task('compress', function (cb) {
-    pump([
-          gulp.src('lib/resources/javascript/*.js'),
-          uglify(),
-          gulp.dest('dist')
-      ],
-      cb
-    );
+gulp.task("sass", function() {
+  return gulp
+    .src("./lib/resources/css/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("dist/css"))
+    .pipe(gulp.dest("lib/resources/css"));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./lib/resources/css/scss/*.scss', ['sass']);
   });
+
+
